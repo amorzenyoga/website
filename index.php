7a4957146d6a8a8d38f9f721c23797c6a4c80848
ABK <Anoluck@Gmail.Com>
2017-01-05 12:27:49 -0500

    updating index.php

--- index.php
+++ index.php
@@ -24,13 +24,13 @@
         <div class="header-container">
             <header class="wrapper clearfix">
                 <h1 class="title">h1.title</h1>
-                <nav>
+                <!-- <nav>
                     <ul>
                         <li><a href="#">nav ul li a</a></li>
                         <li><a href="#">nav ul li a</a></li>
                         <li><a href="#">nav ul li a</a></li>
                     </ul>
-                </nav>
+                </nav> -->
             </header>
         </div>
 
